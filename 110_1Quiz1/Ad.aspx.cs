f0b97e00a6e96db82f57c596b0b00f20864d1fb0
俊瑋 黃 <108111105@mail.aeust.edu.tw>
2021-12-03 18:43:47 +0800

    隨堂1

--- 110_1Quiz1/Ad.aspx.cs
+++ 110_1Quiz1/Ad.aspx.cs
@@ -5,9 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _110_1Quiz1 {
-    public partial class Ad : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace _110_1Quiz1
+{
+    public partial class Ad : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
         }
     }
